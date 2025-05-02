4763fd4edcd0043b2882360bcff34b0f23fa8bf0
Kyehor1990 <ardauludag2005@gmail.com>
2025-05-02 21:41:58 +0300

    .

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,58 +1,29 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyChase : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] float chaseSpeed = 4f;
-    [SerializeField] float returnSpeed = 2f;
+    [SerializeField] float chaseDistance = 10f;
 
     private NavMeshAgent agent;
-    private Vector3 startPosition;
-    private bool isPlayerInSight = false;
-    private bool isChasing = false;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        startPosition = transform.position;
     }
 
     void Update()
     {
-        if (isPlayerInSight)
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance < chaseDistance)
         {
-            agent.speed = chaseSpeed;
             agent.SetDestination(player.position);
-            isChasing = true;
         }
-        else if (isChasing)
+        else
         {
-            agent.speed = returnSpeed;
-            agent.SetDestination(startPosition);
-
-            if (Vector3.Distance(transform.position, startPosition) < 0.5f)
-            {
-                isChasing = false;
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Trigger Entered: " + other.name);
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player detected!");
-            isPlayerInSight = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerInSight = false;
+            agent.ResetPath();
         }
     }
 }
