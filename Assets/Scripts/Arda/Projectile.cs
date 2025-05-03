4b65e140dcc994bde822079cd4c98dbee0ad91aa
Kyehor1990 <ardauludag2005@gmail.com>
2025-05-03 06:42:21 +0300

    .

---
+++ Assets/Scripts/Arda/Projectile.cs
@@ -0,0 +1,42 @@
+using Mehmet;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = transform.forward * speed;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            EnemyStunnable enemy = collision.gameObject.GetComponent<EnemyStunnable>();
+            if (enemy != null)
+            {
+                enemy.Stun(4f);
+            }
+        }
+
+        if (collision.collider.CompareTag("EnemyWorker"))
+        {
+            EnemyWorker enemy = collision.gameObject.GetComponent<EnemyWorker>();
+            if (enemy != null)
+            {
+                enemy.Stun2(4f);
+            }
+        }
+
+        if (!collision.collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
