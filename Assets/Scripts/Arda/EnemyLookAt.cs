fcae12255f1fdd53eb1948dce564d81a3ca4abeb
Kyehor1990 <ardauludag2005@gmail.com>
2025-05-03 03:17:30 +0300

    .

---
+++ Assets/Scripts/Arda/EnemyLookAt.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyLookAt : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float rotationSpeed;
+
+    void Update()
+    {
+        Vector3 directionToPlayer = player.position - transform.position;
+        
+        directionToPlayer.y = 0;
+
+        if (directionToPlayer.magnitude > 0.1f) 
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
