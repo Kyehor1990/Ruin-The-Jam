a8e39da0de53401e0fc3c5ecc2013cdb1921aac8
Mehmet Ali Atik <alimehmetatik@gmail.com>
2025-05-03 08:09:05 +0300

    .

--- Assets/Scripts/Mehmet/PickUpTrigger.cs
+++ Assets/Scripts/Mehmet/PickUpTrigger.cs
@@ -23,6 +23,32 @@ public class PickUpTrigger : MonoBehaviour
     void PickUp()
     {
         Debug.Log("Pickup işlemi gerçekleşti!");
+        Debug.Log("Pickup işlemi başladı!");
+
+        foreach (Transform child in transform)
+        {
+            switch (child.name)
+            {
+                case "1":
+                    DoFunctionOne();
+                    break;
+                case "2":
+                    DoFunctionTwo();
+                    break;
+                case "3":
+                    DoFunctionThree();
+                    break;
+                case "4":
+                    DoFunctionFour();
+                    break;
+                case "5":
+                    DoFunctionFive();
+                    break;
+                default:
+                    Debug.Log("Bilinmeyen obje ismi: " + child.name);
+                    break;
+            }
+        }
         // Buraya ses efekti, animasyon, puan vs. eklenebilir
     }
 
@@ -37,5 +63,35 @@ public class PickUpTrigger : MonoBehaviour
 
         Destroy(topParent.gameObject);
     }
+    void DoFunctionOne()
+    {
+        Debug.Log("Fonksiyon 1 çağrıldı: Örneğin can ver.");
+        // playerHealth += 10;
+    }
+
+    void DoFunctionTwo()
+    {
+        Debug.Log("Fonksiyon 2 çağrıldı: Mermi ver.");
+        // playerAmmo += 5;
+    }
+
+    void DoFunctionThree()
+    {
+        Debug.Log("Fonksiyon 3 çağrıldı: Altın ver.");
+        // playerGold += 100;
+    }
+
+    void DoFunctionFour()
+    {
+        Debug.Log("Fonksiyon 4 çağrıldı: Zıplama gücü artır.");
+        // playerJumpPower *= 1.2f;
+    }
+
+    void DoFunctionFive()
+    {
+        Debug.Log("Fonksiyon 5 çağrıldı: Zamanı yavaşlat.");
+        // Time.timeScale = 0.5f;
+    }
+
 }
 
