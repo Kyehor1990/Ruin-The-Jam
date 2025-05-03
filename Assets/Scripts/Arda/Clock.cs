f519d4e06256769377d8ea82f162adbd1a7b7233
Kyehor1990 <ardauludag2005@gmail.com>
2025-05-03 03:40:10 +0300

    .

---
+++ Assets/Scripts/Arda/Clock.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public float startTime = 8f;
+    public float endTime = 20f;
+    private float currentTime;
+    private float timeToComplete = 300f;
+    private float timeElapsed = 0f;
+
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    void Update()
+    {
+        if (currentTime < endTime)
+        {
+            timeElapsed += Time.deltaTime;
+
+            float timeRatio = timeElapsed / timeToComplete;
+
+            currentTime = startTime + (timeRatio * (endTime - startTime));
+
+            int hours = Mathf.FloorToInt(currentTime); // Saat
+            int minutes = Mathf.FloorToInt((currentTime - hours) * 60f); // Dakika
+
+            timeText.text = string.Format("{0:D2}:{1:D2}", hours, minutes);
+        }
+        else
+        {
+            timeText.text = "20:00";
+        }
+    }
+}
