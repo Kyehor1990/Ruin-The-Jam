d929859c9d04265f9763a37a75185becf5b4fd62
Kyehor1990 <ardauludag2005@gmail.com>
2025-05-03 08:32:07 +0300

    .

--- Assets/Scripts/Arda/EnemyLookAt.cs
+++ Assets/Scripts/Arda/EnemyLookAt.cs
@@ -2,26 +2,8 @@ using UnityEngine;
 
 public class EnemyLookAt : MonoBehaviour
 {
-    [SerializeField] Transform player;
-    [SerializeField] float rotationSpeed;
-
-    void Start()
-{
-    GameObject myObject = GameObject.FindGameObjectWithTag("MainCamera");
-
-    player = myObject.transform;
-}
-
     void Update()
     {
-        Vector3 directionToPlayer = player.position - transform.position;
-        
-        directionToPlayer.y = 0;
-
-        if (directionToPlayer.magnitude > 0.1f) 
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
     }
 }
